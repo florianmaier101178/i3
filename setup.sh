6ae2b19182b37258cf1f3e427f73c9e768cf6b04
Maier Florian (HAU-EE) <florian.maier2@liebherr.com>
2018-08-29 06:49:02 +0200

    added helper commands

--- setup.sh
+++ setup.sh
@@ -9,3 +9,7 @@ fi
 # link i3 config
 ln -s ~/i3/i3config ~/.config/i3/config
 
+# link i3 helper commands
+sudo ln -s ~/i3/bin/moveWorkspaceRight.sh /usr/local/bin/moveWorkspaceRight
+sudo ln -s ~/i3/bin/moveWorkspaceLeft.sh /usr/local/bin/moveWorkspaceLeft
+
