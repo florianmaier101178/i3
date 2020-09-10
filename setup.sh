9d0ec1dca37e49a7567178c4e2a7d8e6b8827c0f
Florian Maier <florian.maier2@liebherr.com>
2020-09-10 23:35:52 +0200

    i3status configuration

--- setup.sh
+++ setup.sh
@@ -9,6 +9,9 @@ fi
 # link i3 config
 ln -s ~/i3/i3config ~/.config/i3/config
 
+# link i3 config
+ln -s ~/i3/i3status.conf ~/.config/i3/i3status.conf
+
 # link i3 helper commands
 sudo ln -s ~/i3/bin/moveWorkspaceRight.sh /usr/local/bin/moveWorkspaceRight
 sudo ln -s ~/i3/bin/moveWorkspaceLeft.sh /usr/local/bin/moveWorkspaceLeft
