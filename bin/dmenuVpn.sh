2274091fa99f7f969395c88b5a9e5ac0a920288f
Florian Maier <florian.maier2@liebherr.com>
2020-09-16 06:16:28 +0200

    better handling of remmina rdp connections

--- bin/dmenuVpn.sh
+++ bin/dmenuVpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-vpn_commands="connect\ndisconnect\nstatus"
+vpn_commands="connect\ndisconnect\nstatus\nintra"
 
 vpn_command=$(echo -e "$vpn_commands" | dmenu -i -p "VPN:")
 
@@ -13,6 +13,9 @@ case "$vpn_command" in
         ;;
     status)
         notify-send -t 3000 "$(lhvpns)"
+        ;;
+    intra)
+        remmina -c ~/.local/share/remmina/group_rdp_liebherr-intra_10-4-8-157.remmina
         ;;
 esac
 
