4c9abae1ee6f9962cd6f2b71e89bed759622c60f
Florian Maier <florian.maier2@liebherr.com>
2020-09-23 19:10:17 +0200

    fixed bug in script

--- py3status/lanStatus.sh
+++ py3status/lanStatus.sh
@@ -4,17 +4,17 @@
 
 ip addr show enx98e743e24afa | grep "state UP" > /dev/null
 liebherr_lan_status=$(echo $?)
-liebherr_ip_address=$(ip addr show enx98e743e24afa | grep 'inet ' | awk '{print $2}' | cut -f1 -d"/")
 if [ $liebherr_lan_status -eq 0 ]; then
+    liebherr_ip_address=$(ip addr show enx98e743e24afa | grep 'inet ' | awk '{print $2}' | cut -f1 -d"/")
     printf " $liebherr_ip_address"
     exit 0
 fi
 
-ip addr show eno2 | grep "state UP"
+ip addr show eno2 | grep "state UP" > /dev/null
 home_lan_status=$(echo $?)
-home_ip_address=$(ip addr show eno2 | grep 'inet ' | awk '{print $2}' | cut -f1 -d"/")
 if [ $home_lan_status -eq 0 ]; then
-    printf "  home_ip_address"
+    home_ip_address=$(ip addr show eno2 | grep 'inet ' | awk '{print $2}' | cut -f1 -d"/")
+    printf " $home_ip_address"
     exit 0
 fi
 
