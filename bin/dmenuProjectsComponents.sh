f4e5dc71c14d715a10368e6b336f2d05cf8f4dd7
Florian Maier <florian.maier2@liebherr.com>
2021-09-14 15:04:03 +0200

    itest for dmenu projects

--- bin/dmenuProjectsComponents.sh
+++ bin/dmenuProjectsComponents.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-projects_commands="iaac\niaac-initial\njenkins-slaves"
+projects_commands="shaq\nstamford\nzizou\nitest\nfrodeno\niaac\niaac-generic\niaac-initial\njenkins-shared-library\njenkins-agent-containers"
 
 project_command=$(echo -e "$projects_commands" | dmenu -i -p "Projects:")
 
-cd /home/flo/projects/cot/$project_command && idea .
+if [ $project_command == "shaq" ]; then
+    cd /home/flo/projects/cot/$project_command && goland .
+elif [ $project_command == "stamford" ]; then
+    cd /home/flo/projects/cot/$project_command && goland .
+elif [ $project_command == "itest" ]; then
+    cd /home/flo/projects/cot/$project_command && goland .
+else
+    cd /home/flo/projects/cot/$project_command && idea .
+fi
 
--- py3status/k8senv.sh
+++ py3status/k8senv.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-CONFIG_FILE=$(readlink /home/flo/.kube/config)
+PROJECT=$(kubecfg project show | head -n -1 | tail -n +2 | xargs)
+CONTEXT=$(kubecfg context show | head -n -1 | tail -n +2 | xargs)
 
-if [ $CONFIG_FILE == "/home/flo/.kube/config_haubusraspberrypiaks" ]; then
-    printf "pi"
-else
-    cat /home/flo/.kube/config | grep "cluster: haubus" | awk '{print $2}' | sed 's/.\{3\}$//' | sed 's/^.\{6\}//' | awk '{printf $1}'
-fi
-
+printf "\n $PROJECT - $CONTEXT\n"
 
