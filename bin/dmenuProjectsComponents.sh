b1af29a0ec7514a6bec2a93d10350ee08b09cf87
Florian Maier <florian.maier2@liebherr.com>
2021-09-14 16:16:54 +0200

    don't open all projects in case of escape the input, for dmenu project opening

--- bin/dmenuProjectsComponents.sh
+++ bin/dmenuProjectsComponents.sh
@@ -4,7 +4,10 @@ projects_commands="shaq\nstamford\nzizou\nitest\nfrodeno\niaac\niaac-generic\nia
 
 project_command=$(echo -e "$projects_commands" | dmenu -i -p "Projects:")
 
-if [ $project_command == "shaq" ]; then
+# the first line in the if cascade check the variable against "empty or spaces string", this handles pressing the escape key ;-)
+if [[ -z "${project_command// }" ]]; then
+    notify-send -t 2000 "escape key opens no project ;-)"
+elif [ $project_command == "shaq" ]; then
     cd /home/flo/projects/cot/$project_command && goland .
 elif [ $project_command == "stamford" ]; then
     cd /home/flo/projects/cot/$project_command && goland .
