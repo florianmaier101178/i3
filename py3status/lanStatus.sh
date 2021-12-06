e55da529e0b7fe51f9a9bdb82018f4265ef30bf9
Florian Maier <florian.maier2@liebherr.com>
2021-12-06 15:53:40 +0100

    changed ip address icon

--- py3status/lanStatus.sh
+++ py3status/lanStatus.sh
@@ -6,7 +6,8 @@ ip addr show enp0s31f6 | grep "state UP" > /dev/null
 liebherr_lan_status=$(echo $?)
 if [ $liebherr_lan_status -eq 0 ]; then
     liebherr_ip_address=$(ip addr show enp0s31f6 | grep 'inet ' | awk '{print $2}' | cut -f1 -d"/")
-    printf " $liebherr_ip_address"
+    #printf "\xef\x80\x87 $liebherr_ip_address"
+    printf "\xef\x80\x87 $liebherr_ip_address"
     exit 0
 fi
 
@@ -14,10 +15,10 @@ ip addr show eno2 | grep "state UP" > /dev/null
 home_lan_status=$(echo $?)
 if [ $home_lan_status -eq 0 ]; then
     home_ip_address=$(ip addr show eno2 | grep 'inet ' | awk '{print $2}' | cut -f1 -d"/")
-    printf " $home_ip_address"
+    printf "\xef\x80\x87 $home_ip_address"
     exit 0
 fi
 
-printf " down"
+printf "\xef\x80\x87 down"
 exit 1
 
